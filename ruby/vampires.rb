2a63f35bc851708fe8eaf397e5b60e9583149b26
Mike Tsou <mr.tsou@gmail.com>
2017-03-07 01:57:07 -0800

    Add release 4 with allergies loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,6 +14,9 @@ while interview <= number_of_employees
 
   puts "What year were you born?"
   year = gets.chomp.to_i
+
+  real_age = Time.now.year - year
+
 # Added a loop to both garlic and insurance questions to ensure only two possible answers.
   puts "Our company cafeteria serves garlic bread. Should we order some for you? Type 'y' or 'n'."
   garlic_bread = gets.chomp.downcase
@@ -29,19 +32,32 @@ while interview <= number_of_employees
     insurance = gets.chomp.downcase
   end
 
-  real_age = Time.now.year - year
-
 # Release 2
   if (real_age == age) && (garlic_bread == "y" || insurance == "y")
     puts "Probably not a vampire."
   elsif (real_age != age) && (garlic_bread == "n" || insurance == "n")
     puts "Probably a vampire."
-  elsif (real_age != age && garlic_bread == "n" && insurance == "n") #This condition is never met because the || boolean on line 39 overrides this condition.
+  elsif (real_age != age && garlic_bread == "n" && insurance == "n") # This condition is never met because the || boolean on line 39 overrides this condition.
     puts "Almost certainly a vampire."
   elsif name == "Drake Cula" || name == "Tu Fang"
     puts "Definitely a vampire."
   else
     puts "Results inconclusive."
   end
+
+# Release 4
+# Not sure if this release is in the correct location in the program. I was going to drop it in between release 1 and 2 but thought this was more logical.
+  puts "List all allergies you might have one at a time. Type 'done' when finished."
+  allergies = gets.chomp.to_s
+
+  while allergies != "done"
+    if allergies == "sunshine"
+      puts "Probably a vampire."
+      break
+    else
+      puts "List all allergies you might have one at a time. Type 'done' when finished."
+      allergies = gets.chomp.to_s
+    end
+  end  
   interview += 1
 end
