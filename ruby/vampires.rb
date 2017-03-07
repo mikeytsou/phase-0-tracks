cc15f887513f76d7a7dc50b26a65a4f49c1f8955
Mike Tsou <mr.tsou@gmail.com>
2017-03-06 23:39:14 -0800

    Add release 2 input variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,16 @@
+
+# Release 1
 puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? Type 'y' or 'n'."
+garlic_bread = gets.chomp 
+puts "Would you like to enroll in the company's health insurance? Type 'y' or 'n'."
+insurance = gets.chomp
 
-puts "How old are you? What year were you born?"
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-
-puts "Would you like to enroll in the company's health insurance?"
+    
+    
