c3743ae060d0674a1d0040904a5a3ab18362043c
Mike Tsou <mr.tsou@gmail.com>
2017-03-06 22:06:49 -0800

    Add release 1 interview questions to vampire.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+
+puts "How old are you? What year were you born?"
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+puts "Would you like to enroll in the company's health insurance?"
