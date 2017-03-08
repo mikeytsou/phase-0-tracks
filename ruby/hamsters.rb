2cb66a3bbce770a154999b97e9abee5a0e9b9ac3
Mike Tsou <mr.tsou@gmail.com>
2017-03-07 16:49:59 -0800

    Add release 1 hamster questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,10 @@
+# Release 1
+puts "What is the hamsters name?"
+
+puts "How loud is the hamsters from a 1 to 10 scale?"
+
+puts "What is the fur color of the hamster?"
+
+puts "Is the hamster a good candidate for adoption? Type 'y' or 'n'."
+
+puts "How old is the hamster?"
