17ef6a90008fed11865b7fee88b2d67cf85d970a
Mike Tsou <mr.tsou@gmail.com>
2017-03-07 16:51:35 -0800

    Add release 2 input variables

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,15 @@
-# Release 1
+# Release 1 & 2
 puts "What is the hamsters name?"
+name = gets.chomp
 
 puts "How loud is the hamsters from a 1 to 10 scale?"
+volume = gets.chomp.to_i
 
 puts "What is the fur color of the hamster?"
+fur_color = gets.chomp
 
 puts "Is the hamster a good candidate for adoption? Type 'y' or 'n'."
+adoption = gets.chomp.downcase
 
 puts "How old is the hamster?"
+age = gets.chomp
