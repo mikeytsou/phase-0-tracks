d0e52ae9e373c577bc6a54b1a75468b88b297653
Mike Tsou <mr.tsou@gmail.com>
2017-03-10 23:11:49 -0800

    Create hash with questions linking keys and values

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -21,7 +21,25 @@ p application
 -Use combinations of ruby objects
 =end
 
+puts "Interior Design Client Information"
+client_info = {}
 
+puts "What is your name?"
+client_info[:name] = gets.chomp
+
+puts "How old are you?"
+client_info[:age] = gets.chomp.to_i
+
+puts "How many children do you have?"
+client_info[:children] = gets.chomp.to_i
+
+puts "Do you offer custom decor themes? Type 'y' or 'n'."
+client_info[:theme] = gets.chomp == 'y' ? true : false
+
+puts "Do you have any experience? Type 'y' or 'n'."
+client_info[:experience] = gets.chomp == 'y' ? true : false
+
+p client_info
 
 
 
