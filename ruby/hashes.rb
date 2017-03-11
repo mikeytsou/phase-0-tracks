4b076c3a03f016c95df310c24baaf2577ce96b96
Mike Tsou <mr.tsou@gmail.com>
2017-03-10 23:09:38 -0800

    Brainstop pseudocode for release 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,41 @@
+# Release 1
+=begin
+puts "INTERIOR DESIGNER JOB APPLICATION"
+application = {
+  name: "sam",
+  address: "1 Fake Drive",
+  email: "fake@email.com",
+  phone: "111-222-3333"
+}
+
+application[:email] = "anotherfake@email.com"
+application[:hire] = "yes"
+
+p application
+=end
+
+# Release 3
+=begin
+-Create empty hash
+-Ask client for details with keys and values
+-Use combinations of ruby objects
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
