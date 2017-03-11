aa4e5aa10e21fefb53bf04adec3156285583dfb8
Mike Tsou <mr.tsou@gmail.com>
2017-03-11 00:38:02 -0800

    Create conditional statement if user needs to update answer

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -33,27 +33,38 @@ client_info[:age] = gets.chomp.to_i
 puts "How many children do you have?"
 client_info[:children] = gets.chomp.to_i
 
-puts "Do you offer custom decor themes? Type 'y' or 'n'."
-client_info[:theme] = gets.chomp == 'y' ? true : false
+puts "What are your decor themes?"
+client_info[:theme] = gets.chomp
 
 puts "Do you have any experience? Type 'y' or 'n'."
 client_info[:experience] = gets.chomp == 'y' ? true : false
 
-p client_info
+puts "Your current information: #{client_info}"
+puts "Name a key you would like to update or type 'none' to complete."
+update = gets.chomp
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if update == 'none'
+  puts "Thanks for providing information"
+elsif update.to_sym == :name
+  puts "What is your name?"
+  client_info[:name] = gets.chomp
+  puts "Your updated information: #{client_info}"
+elsif update.to_sym == :age
+  puts "How old are you?"
+  client_info[:age] = gets.chomp.to_i
+  puts "Your updated information: #{client_info}"
+elsif update.to_sym == :children
+  puts "How many children do you have?"
+  client_info[:children] = gets.chomp.to_i
+  puts "Your updated information: #{client_info}"
+elsif update.to_sym == :theme
+  puts "What are your decor themes?"
+  client_info[:theme] = gets.chomp
+  puts "Your updated information: #{client_info}"
+elsif update.to_sym == :experience
+  puts "Do you have any experience? Type 'y' or 'n'."
+  client_info[:experience] = gets.chomp == 'y' ? true : false
+  puts "Your updated information: #{client_info}"
+end
+  
 
