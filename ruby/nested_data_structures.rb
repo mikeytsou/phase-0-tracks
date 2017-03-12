bbae00aa423bd1d65474880e32e2c50b9fa758f2
Mike Tsou <mr.tsou@gmail.com>
2017-03-12 16:38:33 -0700

    Add driver code to test and access nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,19 +1,29 @@
 # Release 3
 supermarket = {
   checkout: {
-    type: ["cashier", "self checkout"],
+    type: ['cashier', 'self checkout'],
+    bag: ['paper', 'plastic'],
     no_aisles: 10,
     no_atms: 3
   },
-  meat: ["beef", "chicken", "pork", "seafood"],
-  health: ["vitamins", "tooth paste", "soap", "deodorant"],
-  dairy: ["milk", "eggs", "cheese", "yogurt"],
+  meat: [['beef', 'burgers'], ['chicken', 'wings'], ['pork', 'hotdogs'], ['seafood', 'ceviche']],
+  health: ['vitamins', 'tooth paste', 'soap', 'deodorant'],
+  dairy: ['milk', 'eggs', 'cheese', 'yogurt'],
   beverages: {
-    alcoholic: ["vodka", "whiskey", "rum", "beer"],
-    non_alcoholic: ["juice", "soda", "water", ]
+    alcoholic: ['vodka', 'whiskey', 'wine', 'beer'],
+    non_alcoholic: ['juice', 'soda', 'water', 'tea']
   },
   produce: {
-    vegetables: ["carrots", "onions", "celery", "radish"],
-    fruits: ["apples", "oranges", "peaches", "bananas"]
+    vegetables: ['carrots', 'onions', 'celery', 'radish'],
+    fruits: ['apples', 'oranges', 'peaches', 'bananas']
   }
 }
+
+p "Fastest way to checkout: #{supermarket[:checkout][:type][1]}"
+p "Add 'reusable' to bag type: #{supermarket[:checkout][:bag] << 'reusable'}"
+p "Popular seafood dish: #{supermarket[:meat][3][1]}"
+p "Capitalize yogurt in dairy: #{supermarket[:dairy][3].capitalize}"
+p "List all fruits: #{supermarket[:produce][:fruits]}"
+
+
+
