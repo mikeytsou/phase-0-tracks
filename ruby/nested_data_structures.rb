33fc8cb849ad485b644c51fc984ee7c792d37d67
Mike Tsou <mr.tsou@gmail.com>
2017-03-12 15:34:32 -0700

    Create nested data structure with hashes and arrays

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,19 @@
+# Release 3
+supermarket = {
+  checkout: {
+    type: ["cashier", "self checkout"],
+    no_aisles: 10,
+    no_atms: 3
+  },
+  meat: ["beef", "chicken", "pork", "seafood"],
+  health: ["vitamins", "tooth paste", "soap", "deodorant"],
+  dairy: ["milk", "eggs", "cheese", "yogurt"],
+  beverages: {
+    alcoholic: ["vodka", "whiskey", "rum", "beer"],
+    non_alcoholic: ["juice", "soda", "water", ]
+  },
+  produce: {
+    vegetables: ["carrots", "onions", "celery", "radish"],
+    fruits: ["apples", "oranges", "peaches", "bananas"]
+  }
+}
