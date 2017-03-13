9704a67e6bcac1ee9753acfafc95d26df28b23e2
Mike Tsou <mr.tsou@gmail.com>
2017-03-13 14:01:12 -0700

    Pseudocode for release 0 algorithm

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# Release 0
+=begin
+-Define method that loops through string, replace all vowels and consonants, and store result in new variable
+  -Conditionally test if letter in loop is a vowel or consonant
+-Define method that converts string to array, swap and reverse its order, and capitalize
+-Define methods that advance the current letter of vowels and consonants
+-Use methods created to return result
+=end
+
+
+
+
+
+
+
+
