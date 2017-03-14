708d6fd6a24fc91cb2f6d8eeca50d5bf5086967b
Mike Tsou <mr.tsou@gmail.com>
2017-03-14 11:55:11 -0700

    Add release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+# Release 0
 # iterating without parameter
 5.times do
   puts "This is a test"
@@ -8,9 +9,51 @@ end
 end
 # one line block
 4.times {|x| puts x + x}
-# using array
+
+# Release 1
 array = [2, 5, 3, 7, 9]
+
+full_name = {
+  first: "adam",
+  middle: "richard",
+  last: "smith"
+}
+
+puts "array output"
 array.each do |x|
   puts x + 2
 end
 p array
+
+puts "map output"
+new_array = array.map do |x|
+  x * 2
+end
+p new_array
+
+puts "map! output"
+array.map! do |x|
+  x + 10
+end
+p array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
