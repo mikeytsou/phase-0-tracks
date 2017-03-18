4eeb96e6de792a42e1ccafffcd8565eea64a852e
Mike Tsou <mr.tsou@gmail.com>
2017-03-18 13:49:32 -0700

    Create release 0 puppy class with characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Release 0
+=begin
+SPECIES---------------------------
+Puppies
+CHARACTERISTICS-------------------
+Grooming:
+Shedding:
+Breed:
+Size:
+Color:
+BEHAVIOR--------------------------
+Aggressive
+Barking
+Chewing
+=end
