803c8a9b6b83cc31f5f96ac4ffbf4fc8e918453f
Mike Tsou <mr.tsou@gmail.com>
2017-03-21 12:47:07 -0700

    Add and test Puppy class instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+# Release 0 & 1
 class Puppy
 
   def fetch(toy)
@@ -5,5 +6,36 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times {puts "Woof!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_age = 0
+    human_years.times do
+      dog_age += 7
+    end
+    dog_age
+  end
+
+  def play_dead(bang)
+    if bang == "bang" 
+      puts "*playing dead*"
+    end
+  end
+
 end
+
+# DRIVER CODE ----------------------------------------
+
+new_puppy = Puppy.new
+new_puppy.fetch("bone")
+new_puppy.speak(5)
+new_puppy.roll_over
+new_puppy.dog_years(5)
+new_puppy.play_dead("bang")
 
