45cd5b673016e4970aa8e090159bbdc69b399cfe
Mike Tsou <mr.tsou@gmail.com>
2017-03-27 21:04:37 -0700

    Create explore.js file

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+/*
+Release 0
+1. What is the equivalent of nil in JavaScript?
+null
+
+2. When you declare variables in JavaScript, it's a good idea to use the var keyword (you can Google it if you're interested, or just take our word for it). We can see other differences as well: how is the naming convention in JavaScript different from Ruby?
+Variables are declared by using the var keyword the naming convention requires camelCase.
+
+3. How do you print a variable in JavaScript?
+Variables are printed with console.log
+
+4. How do you end a line of code in JavaScript?
+Line of are ended with ;
+
+5. How do you write comments in JavaScript?
+Single line comments //
+Multi line comments /**/
+
