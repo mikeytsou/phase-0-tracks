ef44536ddd6c9077c4e5ef2c559ba8a7b5cbaa6e
Mike Tsou <mr.tsou@gmail.com>
2017-03-28 11:33:05 -0700

    Create new object and push key and value pairs into new object from arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,10 @@ horseColors.push("purple");
 horseNames.push("Doofuss");
 console.log(horseColors);
 console.log(horseNames);
+
+//Release 1
+var horseObjects = {};
+for (var i = 0; i < horseNames.length; i += 1) {
+  horseObjects[horseNames[i]] = horseColors[i];
+}
+console.log(horseObjects);
