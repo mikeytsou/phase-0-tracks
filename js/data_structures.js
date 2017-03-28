d81699af0281bed647d6877744a4c999d44d18ec
Mike Tsou <mr.tsou@gmail.com>
2017-03-28 12:13:08 -0700

    Complete research on my own

--- js/data_structures.js
+++ js/data_structures.js
@@ -24,7 +24,6 @@ function Car(make, model, year) {
 
   console.log(`A ${year} ${make} ${model} was bought!`);
 }
-
 console.log("Let go car shopping");
 
 var newCarOne = new Car("Honda", "Accord", 2014);
@@ -36,3 +35,27 @@ var newCarTwo = new Car("BMW", "5 Series", 2017);
 console.log(newCarTwo)
 console.log("Congrats on your new car, honk your horn!");
 newCarTwo.honk();
+
+/*
+var Car = {
+  make: "Toyota",
+  model: "Prius",
+  price: 20000,
+  buying: function() {
+    if (this.price > 10000) {
+      console.log("This car is too expensive");
+    } else {
+      console.log("This car is in my price range");
+    }
+  }
+};
+*/
+
+//Release 3
+//Iterate through object and returns objects keys and values
+//If an object doesn't have a behavior, literal construction is preferred. If an object has behavior, constructor functions are preferred.
+
+for (var prop in horseObjects) {
+  console.log(prop, "=", horseObjects[prop]);
+}
+
