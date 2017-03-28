4a81686ba54af06d7d90c3eff70c61713c499b30
Mike Tsou <mr.tsou@gmail.com>
2017-03-27 19:00:19 -0700

    Complete release 4, 5, 6, and 7

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -3,63 +3,56 @@
 # I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
 
-# EXPLANATION OF require_relative
-#
-#
+# require_relative loads the file in the same directory of the file that called it
+# require relative method signature returns true if the file is found and returns false
+# if not and will generate a load error.
+# it differs from require because it saves you the effort to provide the path, it's like a syntactic sugar for require
+# also it will not look at your load path folders if it doesn't find the file
+# in the current directory
+
 require_relative 'state_data'
 
 class VirusPredictor
-
+# creating the VirusPredictor object and initializes the instance with it's variables and initial values.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+# calls the methods predicted_deaths and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    puts "#{@state} will lose #{predicted_deaths} people in this outbreak and will spread across the state in #{speed_of_spread} months.\n\n"
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+#calculating the number of deaths depending on population density
+# to predict the severity of the outbreak
+# it produces a printout of the findings but returns nothing
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
+    number_of_deaths = case @population
+    when 200..Float::INFINITY then (@population * 0.4).floor 
+    when 150..199 then (@population * 0.3).floor
+    when 100..149 then (@population * 0.2).floor
+    when 50..99 then (@population * 0.1).floor
+    else (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
-
-  def speed_of_spread(population_density, state) #in months
+# calculates how fast the virus spreads depending on density
+# it will print out the findings and return nil
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    case @population_density
+    when 200..Float::INFINITY then speed += 0.5
+    when 150..199 then speed += 1
+    when 100..149 then speed += 1.5
+    when 50..99 then speed += 2
+    else speed += 2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
+    speed
   end
 
 end
@@ -82,6 +75,8 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-
+STATE_DATA.each do |state, report|
+  VirusPredictor.new(state, report[:population_density], report[:population]).virus_effects
+end
 #=======================================================================
 # Reflection Section
