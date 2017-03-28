539646b373d31208d32a25fda739a392fd9825fb
Mike Tsou <mr.tsou@gmail.com>
2017-03-28 11:31:11 -0700

    Create and push new items in arrays

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+//Release 0
+var horseColors = ["blue", "red", "yellow", "green"];
+var horseNames = ["Lucky Seven", "Seabiscuit", "Ruffian", "Racecar"];
+
+horseColors.push("purple");
+horseNames.push("Doofuss");
+console.log(horseColors);
+console.log(horseNames);
