5180ea5989360a43b70c6576c4d0dc7adccbbd81
Mike Tsou <mr.tsou@gmail.com>
2017-03-31 15:22:16 -0700

    Create function that searches an array for its longest element

--- js/algos.js
+++ js/algos.js
@@ -9,3 +9,24 @@
 // - return longest string variable
 // Output: longest string
 
+function longestPhrase(array) {
+  var longestWord = null;
+
+  for (var i = 0; i < array.length; i += 1){
+    var currentWord = array[i];
+
+    if (longestWord == null) {
+      longestWord = currentWord;
+    } else if (currentWord.length > longestWord.length) {
+      longestWord = currentWord;
+    }
+  }
+  return longestWord
+}
+
+// DRIVERCODE
+var holidays = ["labor", "veterans", "thanksgiving", "christmas"];
+var teams = ["warriors", "niners", "raiders", "athletics", "giants"];
+
+console.log(longestPhrase(holidays));
+console.log(longestPhrase(teams));
