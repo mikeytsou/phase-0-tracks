cbfff7f28e6aeecb54c5f1e6a104b0cc7c38a1d5
Mike Tsou <mr.tsou@gmail.com>
2017-03-31 14:28:40 -0700

    Pseudocode for longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Release 0
+// Method that takes an array of strings and returns the longest string
+// Input: array of strings
+// - variable for current string
+// - variable for longest string
+// - iterate through each element in array
+// - assign first element as current string variable
+// - count length of each element and assign to longest string variable if longer than current string variable
+// - return longest string variable
+// Output: longest string
+
