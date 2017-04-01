b8f9860acfd9d4f7050c2a84ff1bedd45d1cef6e
Mike Tsou <mr.tsou@gmail.com>
2017-04-01 11:36:07 -0700

    Pseudocode for finding match function

--- js/algos.js
+++ js/algos.js
@@ -1,32 +1,35 @@
 // Release 0
 // Method that takes an array of strings and returns the longest string
 // Input: array of strings
-// - variable for current string
 // - variable for longest string
 // - iterate through each element in array
-// - assign first element as current string variable
-// - count length of each element and assign to longest string variable if longer than current string variable
+// - if current element length is longer than longest string variable, current element is assigned to longest string variable
 // - return longest string variable
 // Output: longest string
 
-function longestPhrase(array) {
-  var longestWord = null;
+function longestWord(array) {
+  var longestWord = array[0];
 
-  for (var i = 0; i < array.length; i += 1){
-    var currentWord = array[i];
-
-    if (longestWord == null) {
-      longestWord = currentWord;
-    } else if (currentWord.length > longestWord.length) {
-      longestWord = currentWord;
+  for (var i = 0; i < array.length; i += 1) {
+    if (array[i].length > longestWord.length) {
+      longestWord = array[i];
     }
   }
   return longestWord
 }
 
-// DRIVERCODE
+// DRIVER CODE
 var holidays = ["labor", "veterans", "thanksgiving", "christmas"];
 var teams = ["warriors", "niners", "raiders", "athletics", "giants"];
 
-console.log(longestPhrase(holidays));
-console.log(longestPhrase(teams));
+console.log(longestWord(holidays));
+console.log(longestWord(teams));
+
+// Release 1
+// Method that takes two objects and see if they share key-value pairs
+// Input: two objects
+// - iterate through object one keys
+// - if object two includes same key as object one and object one and object two include the same value
+// - return true
+// Output: returns true or false
+
