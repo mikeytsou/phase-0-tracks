a4ddace68691a8ba126bdc70dbfa3ccb164ee7fe
Mike Tsou <mr.tsou@gmail.com>
2017-04-08 14:05:56 -0700

    Create interface method that sets up program with log in or account creation

--- databases/build_something_awesome/account.rb
+++ databases/build_something_awesome/account.rb
@@ -18,7 +18,42 @@ def program_intro(db)
   action
 end
 
-
+def program_setup(db, action)
+  date = Time.now
+  if action == 2 # Create Account
+    puts "Create a username:"
+    valid_name = false  
+    until valid_name == true
+      username = gets.chomp.downcase
+      puts "*" * 100
+      if check_existing_user(db, username)
+        puts "Enter another username, that one is taken."
+      else
+        valid_name = true
+      end
+    end
+    puts "How much money would you like to initially deposit? (ex. 100.00)"
+    initial_deposit = gets.chomp.to_f
+    puts "*" * 100
+    create_new_user(db, username, date, initial_deposit)
+    print_balance_log(db, username)
+  elsif action == 1 # Log In
+    puts "Enter your username:"
+    valid_name = false
+    until valid_name == true
+      username = gets.chomp.downcase
+      puts "*" * 100
+      if check_existing_user(db, username)
+        print_balance_log(db, username)
+        valid_name = true
+      else
+        puts "Username doesn't exist. Try again."
+      end
+    end
+  end
+  username
+end
 
 db = create_database
 action = program_intro(db)
+username = program_setup(db, action)
