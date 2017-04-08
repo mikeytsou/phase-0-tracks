a8065ade5547f689860323d3d9460e9c9204dcc8
Mike Tsou <mr.tsou@gmail.com>
2017-04-08 14:10:15 -0700

    Create interface method that navigates program menu with view, deposit, withdraw

--- databases/build_something_awesome/account.rb
+++ databases/build_something_awesome/account.rb
@@ -54,6 +54,44 @@ def program_setup(db, action)
   username
 end
 
+def program_navigate(db, username)
+  date = Time.now
+  puts "*" * 100
+  action = nil
+  until action == 4
+    puts "What would you like to do?\n1. View account history\n2. Make a deposit\n3. Make a withdrawal\n4. Exit"
+    action = gets.chomp.to_i
+    puts "*" * 100    
+    if action == 4 # Exit program
+      puts "Goodbye!"
+    elsif action == 3 # Make a withdrawal
+      deposit = false
+      print_balance_log(db, username)
+      puts "*" * 100
+      puts "How much money would you like to withdraw? (ex. 100.00)"
+      withdraw_amount = gets.chomp.to_f
+      puts "*" * 100
+      transaction(db, username, date, withdraw_amount, deposit)
+      print_balance_log(db, username)
+      puts "*" * 100
+    elsif action == 2 # Make a deposit
+      deposit = true
+      print_balance_log(db, username)
+      puts "*" * 100
+      puts "How much money would you like to deposit (ex. 100.00)"
+      deposit_amount = gets.chomp.to_f
+      puts "*" * 100
+      transaction(db, username, date, deposit_amount, deposit)
+      print_balance_log(db, username)
+      puts "*" * 100
+    elsif action == 1 # View account history
+      print_account_log(db, username)
+      puts "*" * 100
+    end
+  end
+end
+
 db = create_database
 action = program_intro(db)
 username = program_setup(db, action)
+program_navigate(db, username)
