93c11fad7d0909231f2fb9626764c90978898209
Mike Tsou <mr.tsou@gmail.com>
2017-04-08 09:49:45 -0700

    Create interface method that starts the program

---
+++ databases/build_something_awesome/account.rb
@@ -0,0 +1,6 @@
+require "sqlite3"
+require_relative "account_methods"
+
+db = create_database
+
+start_program(db)
--- databases/build_something_awesome/account_methods.rb
+++ databases/build_something_awesome/account_methods.rb
@@ -86,3 +86,74 @@ def print_account_log(db, username)
   end
 end
 
+# USER INTERFACE
+def start_program(db)
+  date = Time.now
+  puts "Welcome to Global Bank"
+  puts "-" * 100  
+  action = nil
+  until action == 1 || action == 2 || action == 3
+    puts "What would you like to do?\n1. Log In\n2. Create Account\n3. Exit"
+    puts "-" * 100
+    action = gets.chomp.to_i
+  end
+  
+  if action == 3 # Exit
+    puts "Goodbye~"
+
+  elsif action == 2 # Create Account
+    puts "Create a username:"
+    valid_name = false  
+    until valid_name == true
+      username = gets.chomp.downcase
+      if check_existing_user(db, username)
+        puts "Enter another username, that one is taken."
+      else
+        valid_name = true
+      end
+    end
+    puts "How much money would you like to initially deposit? (ex. 100.00)"
+    initial_deposit = gets.chomp.to_f
+    create_new_user(db, username, date, initial_deposit)
+
+  elsif action == 1 # Log In
+    puts "Enter your username:"
+    valid_name = false
+    until valid_name == true
+      username = gets.chomp.downcase
+      if check_existing_user(db, username)
+        valid_name = true
+      else
+        puts "Username doesn't exist. Try again."
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
